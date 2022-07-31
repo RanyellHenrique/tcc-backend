d7d7207025421b5f42a69bb0267b5e824ec0ca15
ranyellhenrique <ranyellhenrique@gmail.com>
2022-07-30 21:58:27 -0300

    Configuration of authentication

--- src/main/java/br/com/unip/tcc/config/ResourceServerConfig.java
+++ src/main/java/br/com/unip/tcc/config/ResourceServerConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -19,6 +18,9 @@ import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+
 @Configuration
 @EnableResourceServer
 @RequiredArgsConstructor
@@ -27,9 +29,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     private final Environment env;
     private final JwtTokenStore tokenStore;
 
-    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**", "/usuarios/**", "/trabalhadores/**"};
-    private static final String[] OPERATOR_OR_ADMIN = {"/categorias/**"};
-    private static final String[] ADMIN = {"/categorias/**"};
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
+    private static final String[] ADMIN = {"/categorias/**", "/usuarios/**",};
+    private static final String[] CLIENTE = {"/ofertas/**"};
+    private static final String[] PUBLIC_ONLY_READING = {"/categorias/**", "/ofertas/**"};
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
@@ -45,9 +48,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
-                .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
-                .antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
-                .antMatchers(ADMIN).hasAnyRole("ADMIN")
+                .antMatchers(GET, PUBLIC_ONLY_READING).permitAll()
+                .antMatchers(CLIENTE).hasAnyAuthority("CLIENTE")
+                .antMatchers(ADMIN).hasAnyAuthority("ADMIN")
                 .anyRequest().authenticated();
 
         http.cors().configurationSource(corsConfigurationSource());
@@ -56,10 +59,14 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOriginPatterns(Arrays.asList("*"));
+        corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
         return source;
     }
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,23 @@
-INSERT INTO  tb_usuario (id, email, senha) VALUES (91119, 'ana@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO  tb_perfil (id, descricao) VALUES (1, 'ADMIN');
+INSERT INTO  tb_perfil (id, descricao) VALUES (2, 'CLIENTE');
+INSERT INTO  tb_perfil (id, descricao) VALUES (3, 'TRABALHADOR');
+INSERT INTO  tb_usuario (id, email, senha, nome) VALUES (91120, 'ana@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 'Ana');
+INSERT INTO  tb_usuario (id, email, senha, nome) VALUES (91121, 'bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 'Bob');
+INSERT INTO  tb_usuario (id, email, senha, nome) VALUES (91122, 'ranyell@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 'Ranyell');
+INSERT INTO  tb_cliente (cnpj, id) VALUES ('123456', 91120);
+INSERT INTO  tb_trabalhador (cpf, id) VALUES ('123456', 91121);
+INSERT INTO  tb_usuario_perfil (usuario_id, perfil_id) VALUES (91120, 2);
+INSERT INTO  tb_usuario_perfil (usuario_id, perfil_id) VALUES (91121, 3);
+INSERT INTO  tb_usuario_perfil (usuario_id, perfil_id) VALUES (91122, 1);
 INSERT INTO  tb_categoria ( nome, descricao) VALUES ( 'Desenvolvedor Java', 'Programa e desenvolve, implanta sistemas em linguagem Java');
 INSERT INTO  tb_categoria ( nome, descricao) VALUES ( 'Desenvolvedor Python', 'Programa e desenvolve, implanta sistemas em linguagem Python');
 INSERT INTO  tb_categoria ( nome, descricao) VALUES ( 'Desenvolvedor PHP', 'Programa, desenvolve e implanta sistemas em linguagem PHP');
 INSERT INTO  tb_categoria ( nome, descricao) VALUES ( 'Desenvolvedor Java Script', 'Programa, desenvolve e implanta sistemas em linguagem Java Script');
 INSERT INTO  tb_categoria ( nome, descricao) VALUES ( 'Back-end', 'Programa, desenvolve e implanta sistemas Back-end');
 INSERT INTO  tb_categoria ( nome, descricao) VALUES ( 'Front-end', 'Programa, desenvolve e implanta sistemas Front-end');
+INSERT INTO  tb_trabalhador_categoria ( trabalhador_id, categoria_id ) VALUES ( 91121, 1 );
+INSERT INTO  tb_trabalhador_categoria ( trabalhador_id, categoria_id ) VALUES ( 91121, 2 );
+
 
 
 
